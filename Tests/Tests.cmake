26ea66053d8d1767dc804eb2c93ef0125cba9c03
Antoine DESRUET <antoine.desruet@epitech.eu>
2023-08-05 23:08:25 +0200

    <type>: <description>

--- Tests/Tests.cmake
+++ Tests/Tests.cmake
@@ -3,8 +3,6 @@ set(TESTS_SRC
         Tests/basic_assertions.cpp
         Tests/basic_parameterized.cpp
         )
-
-set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} --coverage)
+set(CMAKE_CXX_FLAGS " ${CMAKE_CXX_FLAGS} -fprofile-arcs -ftest-coverage")
 add_executable(${TESTS_NAME} ${TESTS_SRC} ${SRC})
-
 target_link_libraries(${TESTS_NAME} criterion)
